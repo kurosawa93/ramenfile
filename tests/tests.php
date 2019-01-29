04c1c94416dd16b9647a11a27455033d412a8d32
Rachmat Priambudi <rachmat.keybladers93@gmail.com>
2019-01-29 15:40:05 +0700

    change package name to tests firs

---
+++ tests/tests.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Ordent\RamenFile\Controllers\FilesController;
+use Ordent\RamenFile\Model\FileModel;
+use Ordent\RamenFile\Processor\FileProcessor;
+
+echo "Class successfully loaded";
